3fcc613db45ab14545657e82f835bc642ddce832
prathameshpatil7 <patil444411@gmail.com>
2022-12-17 19:19:56 +0530

    made folder structure

---
+++ personal-portfolio/src/components/experience/Experience.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './experience.scss'
+
+function Experience() {
+  return (
+    <div>Experience</div>
+  )
+}
+
+export default Experience
---
+++ personal-portfolio/src/components/header/HeaderSocial.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeaderSocial() {
+  return (
+    <div className="header_socials">
+        <a href="https://www.linkedin.com/in/patilprathamesh6/">LI</a>
+    </div>
+  )
+}
+
+export default HeaderSocial
